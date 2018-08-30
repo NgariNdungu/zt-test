d8ad57c6e53822dd90868f115b9c0789632323d0
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-08-30 19:50:04 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # validations
+  validates :email, :phone, :password_digest, presence: true
+  validates :email, :phone, uniqueness: true
+  validates :phone, numericality: { only_integer: true }
+  
 end
