60a6fcbac9fe6478117f5ba965672e9ba639594d
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-08-31 08:32:48 +0300

    add balance column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180831051621) do
+ActiveRecord::Schema.define(version: 20180831052405) do
 
   create_table "deposits", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20180831051621) do
     t.string   "phone"
     t.string   "name"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "balance",         default: 0
   end
 
 end
