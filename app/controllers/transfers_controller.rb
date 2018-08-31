124b8c90c85838f787ecb328b57e84af4aea39ea
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-08-31 16:02:13 +0300

    update balances on transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,24 +1,37 @@
 class TransfersController < ApplicationController
   before_action :authenticate_user
 
-  def create
-    @transfer = current_user.transfers.new(transfer_params)
+  def create  
+    if params[:email]
+      @recipient = User.find_by(email: params[:email])
+    elsif params[:phone]
+      @recipient = User.find_by(phone: params[:phone])
+    else
+      render json: { "messages": "email or phone is required" }, status: :bad_request
+      return
+    end
+    amount = params[:amount] # if current_user.balance > params[:amount].to_i 
+    @transfer = current_user.sends.new(
+      'recipient_id': @recipient.id,
+      'amount': amount
+    )
     if @transfer.save
       render json: @transfer
     else
-      render json: @transfer.errors.messages
+      render json: @transfer.errors.messages, status: :bad_request
     end
   end
 
   def index
     # method should send email of user transfers
-    render status: ':accepted'
+    render json: { "message": "Email sent" }, status: :accepted
   end
 
   private
   def transfer_params
     params.require(:transfer).permit(
-      :recipient_id,
+      :email,
+      :phone,
       :amount
     )
   end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -4,7 +4,23 @@ class Transfer < ApplicationRecord
   belongs_to :recipient, class_name: 'User'
 
   # validations
-  validates :sender_id, :recipient_id, :amount, presence: true
+  validates :sender_id, :amount, presence: true
+  validates :recipient_id, presence: { message: "Recipient not found" }
   validates :amount, numericality: { only_integer: true }
-  validates :amount, numericality: { less_than: self.sender.balance }
+  validate do |transfer|
+    transfer.errors[:amount] << "You have insufficient funds" if transfer.sender.balance < transfer.amount
+  end
+
+  # update user balances
+  before_create :update_balances
+
+  private
+
+  def update_balances
+    sender_balance = self.sender.balance
+    recipient_balance = self.recipient.balance
+    self.sender.update_attribute(:balance, sender_balance - amount)
+    self.recipient.update_attribute(:balance, recipient_balance + amount)
+  end
+  
 end
